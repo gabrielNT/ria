8d77aa239abab83d694c23ab47ec342003fc5dc4
Gabriel Tutui <gntutui@gmail.com>
2021-07-11 15:54:51 -0300

    Ch3: files - Traits

--- file/src/main.rs
+++ file/src/main.rs
@@ -1,18 +1,38 @@
+//! Rust in Action File implementation
+use std::fmt;
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
-enum FileState {
+pub enum FileState {
     Open,
     Closed,
 }
 
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+/// Represents a "File" living in a file system
 #[derive(Debug)]
-struct File {
-    name: String,
+pub struct File {
+    pub name: String,
     data: Vec<u8>,
-    state: FileState,
+    pub state: FileState,
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} {}>", self.name, self.state)
+    }
 }
 
 impl File {
-    fn new(name: &str) -> File {
+    pub fn new(name: &str) -> File {
         File {
             name: String::from(name),
             data: Vec::new(),
@@ -20,13 +40,13 @@ impl File {
         }
     }
 
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+    pub fn new_with_data(name: &str, data: &Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
         f
     }
 
-    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+    pub fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
         if self.state != FileState::Open {
             return Err(String::from("File must be open for reading"));
         }
@@ -40,12 +60,12 @@ impl File {
     }
 }
 
-fn open(mut f: File) -> Result<File, String> {
+pub fn open(mut f: File) -> Result<File, String> {
     f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(mut f: File) -> Result<File, String> {
+pub fn close(mut f: File) -> Result<File, String> {
     f.state = FileState::Closed;
     Ok(f)
 }
@@ -63,7 +83,8 @@ fn main() {
     f1 = close(f1).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
-    println!("{:?}", f1);
+    println!("Debug: {:?}", f1);
+    println!("Display: {}", f1);
     println!("{} is {} bytes long", &f1.name, f1_length);
     println!("{}", text);
 }
